a4c7d3936c64b0543ff31e90170b68d94dcb9004
Diya Dutta <diya.dutta@esolzmail.com>
2024-07-28 23:34:25 +0530

    add quiz assaignment

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -9,19 +9,23 @@
 <?php $counter = 1;?>
 <section class="mt-5">
     <div class="container">
-        <h1>Start Your Quiz</h1>
-        @foreach($all_quiz as $quizzes)
-        <div class="container mt-2">
-            <div class="row">
-                @if(Auth('member')->check())
-                    <h5><a href="{{route('user.home')}}"><?php echo $counter; ?>. {{$quizzes->question}}</a></h5>
-                @else
-                    <h5><a href="{{route('user.login')}}"><?php echo $counter; ?>. {{$quizzes->question}}</a></h5>
-                @endif
+        @if($all_quiz->count() > 0)
+            <h1>Start Your Quiz</h1>
+            @foreach($all_quiz as $quizzes)
+            <div class="container mt-2">
+                <div class="row">
+                    @if(Auth('member')->check())
+                        <h5><a href="{{route('user.home')}}"><?php echo $counter; ?>. {{$quizzes->question}}</a></h5>
+                    @else
+                        <h5><a href="{{route('user.login')}}"><?php echo $counter; ?>. {{$quizzes->question}}</a></h5>
+                    @endif
+                </div>
             </div>
-        </div>
-        <?php $counter++; ?>
-        @endforeach
+            <?php $counter++; ?>
+            @endforeach
+        @else
+            <h5>No quiz yet.</h5>
+        @endif
     </div>
 </section>
 
