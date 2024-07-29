3d1b8cc17b5f154182dd553a8b4563ae81b403c5
DIYA-784 <125148962+DIYA-784@users.noreply.github.com>
2024-07-29 08:09:16 +0530

    Update login.blade.php

--- resources/views/users/login.blade.php
+++ resources/views/users/login.blade.php
@@ -12,18 +12,18 @@
             @csrf
             
             <div class="form-group">
-                <label for="exampleInputEmail1">Email address</label>
+                <label for="exampleInputEmail1">Email address*</label>
                 <input type="email" class="form-control" id="exampleInputEmail1" name="email" value="{{old('email')}}">
                 @if($errors->has('email'))
-                    <span style="color: red;" class="error-message">{{$errors->first('email')}}</span>
+                    <span style="color: red;">{{$errors->first('email')}}</span>
                 @endif
             </div>
             
             <div class="form-group">
-                <label for="exampleInputPassword1">Password</label>
+                <label for="exampleInputPassword1">Password*</label>
                 <input type="password" class="form-control" id="exampleInputPassword1" name="password">
                 @if($errors->has('password'))
-                    <span style="color: red;" class="error-message">{{$errors->first('password')}}</span>
+                    <span style="color: red;">{{$errors->first('password')}}</span>
                 @endif
             </div>
             
