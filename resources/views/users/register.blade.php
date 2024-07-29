75cd708fad72f954d81c1f2dc7ba9f3779708362
DIYA-784 <125148962+DIYA-784@users.noreply.github.com>
2024-07-29 08:07:54 +0530

    Update register.blade.php

--- resources/views/users/register.blade.php
+++ resources/views/users/register.blade.php
@@ -10,38 +10,38 @@
         <form action="{{route('user.register.create')}}" method="POST">
             @csrf
             <div class="form-group">
-                <label for="exampleInputEmail1">Name</label>
+                <label for="exampleInputEmail1">Name*</label>
                 <input type="text" class="form-control" id="exampleInputEmail1" name="name" value="{{old('name')}}">
                 @if($errors->has('name'))
                     <span style="color: red;">{{$errors->first('name')}}</span>
                 @endif
             </div>
             <div class="form-group">
-                <label for="exampleInputEmail1">Email address</label>
+                <label for="exampleInputEmail1">Email address*</label>
                 <input type="email" class="form-control" id="exampleInputEmail1" name="email" value="{{old('email')}}">
                 @if($errors->has('email'))
                     <span style="color: red;">{{$errors->first('email')}}</span>
                 @endif
             </div>
             <div class="form-group">
-                <label for="exampleInputEmail1">Phone</label>
+                <label for="exampleInputEmail1">Phone*</label>
                 <input type="number" class="form-control" id="exampleInputEmail1" name="phone" value="{{old('phone')}}">
                 @if($errors->has('phone'))
                     <span style="color: red;">{{$errors->first('phone')}}</span>
                 @endif
             </div>
             <div class="form-group">
-                <label for="exampleInputPassword1">Password</label>
+                <label for="exampleInputPassword1">Password*</label>
                 <input type="password" class="form-control" id="exampleInputPassword1" name="password">
                 @if($errors->has('password'))
-                    <span style="color: red;" class="error-message">{{$errors->first('password')}}</span>
+                    <span style="color: red;">{{$errors->first('password')}}</span>
                 @endif
             </div>
             <div class="form-group">
-                <label for="exampleInputPassword1">Confirm Password</label>
+                <label for="exampleInputPassword1">Confirm Password*</label>
                 <input type="password" class="form-control" id="exampleInputPassword1" name="password_confirmation">
                 @if($errors->has('password_confirmation'))
-                    <span style="color: red;" class="error-message">{{$errors->first('password_confirmation')}}</span>
+                    <span style="color: red;">{{$errors->first('password_confirmation')}}</span>
                 @endif
             </div>
             
